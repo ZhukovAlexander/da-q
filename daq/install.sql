67b2a93c037be01ad87703fd472eecde0f727452
Alexander Zhukov <zhukovaa90@gmail.com>
2017-12-04 09:45:22 +0200

    Fix slot turn computation in queue_get

--- daq/install.sql
+++ daq/install.sql
@@ -7,65 +7,89 @@
  * ----------------------------------------------------------------------------
  */
 
-CREATE SEQUENCE IF NOT EXISTS read_ticket;
-CREATE SEQUENCE IF NOT EXISTS write_ticket;
+CREATE OR REPLACE FUNCTION install(capacity integer) RETURNS VOID AS $$
+BEGIN
+    CREATE SEQUENCE IF NOT EXISTS read_ticket  START 0 MINVALUE 0;
+    CREATE SEQUENCE IF NOT EXISTS write_ticket START 0 MINVALUE 0;
+    CREATE TABLE IF NOT EXISTS queue_params (
+        name varchar(64) PRIMARY KEY,
+        value integer
+    );
+    INSERT INTO queue_params (name, value) VALUES
+        ('capacity', capacity) ON CONFLICT (name) DO UPDATE SET value=EXCLUDED.value;
 
-CREATE TABLE IF NOT EXISTS slots (
-	id integer PRIMARY KEY CHECK (id < 64),  -- just hardcode for POC
-	turn integer NOT NULL,
-	value json
-);
+    CREATE TABLE IF NOT EXISTS slots (
+        id integer PRIMARY KEY,
+        turn integer NOT NULL DEFAULT 0,
+        value json
+    );
 
-CREATE OR REPLACE FUNCTION put(val json) RETURNS void AS $$
-DECLARE
-  ticket bigint;
-  turn bigint;
-  slot_id integer;
-  BEGIN
-    SELECT nextval('read_ticket') INTO ticket;
-    SELECT (ticket % 3) INTO slot_id;
-
-    LOOP
-    -- some computations
-        SELECT turn from slots where id=slot_id INTO turn;
-        IF turn=ticket THEN
-            UPDATE slots SET value=val WHERE id=slot_id;
-            EXIT;  -- exit loop
-        END IF;
-    END LOOP;
-    RETURN;
+    INSERT INTO slots (id, turn)
+        SELECT i, i FROM generate_series(0, capacity - 1) AS i
+        ON CONFLICT DO NOTHING;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION get() RETURNS void AS $$
+CREATE OR REPLACE FUNCTION slot(ticket bigint) RETURNS INT AS $slot$
+BEGIN
+    RETURN (SELECT ticket & (value - 1) FROM queue_params where name='capacity');
+END $slot$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION uninstall() RETURNS VOID as $$
+BEGIN
+    DROP TABLE IF EXISTS slots, queue_params;
+    DROP sequence IF EXISTS read_ticket;
+    DROP sequence IF EXISTS write_ticket;
+    -- DROP FUNCTION IF EXISTS queue_put(json);
+    -- DROP FUNCTION IF EXISTS queue_get();
+    -- DROP FUNCTION IF EXISTS slot(bigint);
+END
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION queue_put(val json) RETURNS void AS $$
 DECLARE
   ticket bigint;
-  turn bigint;
+  slot_turn bigint;
   slot_id integer;
-  value json;
+BEGIN
+    SELECT nextval('write_ticket') INTO ticket;
+    SELECT slot(ticket) INTO slot_id;
+
+    LOOP
+        SELECT turn from slots where id = slot_id INTO slot_turn;
+        IF slot_turn = ticket THEN
+            UPDATE slots SET value = val, turn = turn + 1 WHERE id = slot_id;
+          RETURN;
+        END IF;
+    END LOOP;
+END;
+
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION queue_get() RETURNS json AS $result$
+DECLARE
+  ticket bigint;
+  slot_turn bigint;
+  slot_id integer;
+  slot_value json;
+  capacity integer;
 BEGIN
 
-    SELECT nextval('write_ticket') INTO ticket;
-    SELECT (ticket % 3) INTO slot_id;
+    SELECT nextval('read_ticket') INTO ticket;
+    SELECT slot(ticket) INTO slot_id;
+    SELECT value FROM queue_params where name = 'capacity' INTO capacity;
 
     LOOP
-    -- some computations
-        SELECT turn from slots where id=slot_id INTO turn;
-        IF turn=ticket THEN
-            EXIT;  -- exit loop
+        SELECT turn from slots where id = slot_id INTO slot_turn;
+
+        IF slot_turn = ticket + 1 THEN
+            UPDATE slots SET turn = ticket + capacity WHERE id = slot_id RETURNING value INTO slot_value;
+            RETURN slot_value;
         END IF;
     END LOOP;
 
-    LOOP
-    -- some computations
-        SELECT value from slots where id=slot_id INTO value;
-        IF value != null THEN
-            UPDATE slots SET value=null, turn=turn+3+1 WHERE id=slot_id;
-            EXIT;  -- exit loop
-        END IF;
-    END LOOP;
-    RETURN;
 END;
-$$ LANGUAGE plpgsql;
-
-
+$result$ LANGUAGE plpgsql;
